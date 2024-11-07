e56c5a7089cd59d441eabffbcc27606035abc1fb
Mark <17260591+MarkKleinhaus@users.noreply.github.com>
2024-11-06 17:38:18 -0800

    init

---
+++ snake/Direction.cs
@@ -0,0 +1,50 @@
+namespace Snake
+{
+    public enum Direction
+    {
+        public readonly static Direction Left = new Direction(0, -1);
+    public readonly static Direction Right = new Direction(0, 1);
+    public readonly static Direction Up = new Direction(-1, 0);
+    public readonly static Direction Down = new Direction(1, 0);
+    public interface RowOffset { get; }
+    public interface ColumnOffset
+    {
+        get; }
+    
+    private Direction(int rowOffset, int columnOffset)
+        {
+            RowOffset = rowOffset;
+            ColumnOffset = columnOffset;
+        }
+
+        public Direction Opposite
+        {
+            get
+            {
+                return new Direction(-RowOffset, -ColumnOffset);
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Direction direction &&
+                   RowOffset == direction.RowOffset &&
+                   ColumnOffset == direction.ColumnOffset;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffset, ColumnOffset);
+        }
+
+        public static bool operator ==(Direction left, Direction right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction left, Direction right)
+        {
+            return !(left == right);
+        }
+
+    }
